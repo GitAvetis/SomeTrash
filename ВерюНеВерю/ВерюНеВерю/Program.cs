72109dade5babe66426db1abf6bd3974603e173e
GitAvetis <69589264+GitAvetis@users.noreply.github.com>
2020-11-05 18:06:58 +0300

    Add files via upload

---
+++ ВерюНеВерю/ВерюНеВерю/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ВерюНеВерю
+{
+    class Program
+    {
+        static void Main()
+        {            
+            Game( "Questions.csv"); 
+        }
+        static void Game(string csvFile)
+        {
+            IEnumerable<Questions> questionsList = File.ReadAllLines(csvFile)
+                                   .Skip(1)
+                                   .Select(Questions.CsvLineParse)
+                                   .ToList();
+
+            Console.WriteLine("Chose number of question and try to give a correct answer");
+            Console.WriteLine();
+            foreach (var item in questionsList)
+            {
+                Console.WriteLine($"{item.Number}. {item.Question}"); 
+            }
+           
+            int questionNumber = int.Parse(Console.ReadLine());
+
+            Console.Clear();
+            foreach (var item in questionsList.Where(questionNum => questionNum.Number == questionNumber))
+            {
+                Console.WriteLine(item.Question);
+            }
+
+            Console.WriteLine("What do you think, Yes or No? Writе answer under this line ");
+            string playersPrediction = Console.ReadLine().Trim().ToUpper();
+            Console.Clear();
+            foreach (var item in questionsList.Where(questionNum => questionNum.Number == questionNumber))
+            {                  
+                if (item.Answer.Trim().ToUpper() == playersPrediction)
+                    Console.WriteLine($"You're right!");
+                else
+                    Console.WriteLine($"Nope. Actualy answer was {item.Answer}.\n{item.Comment}");
+            }        
+
+        }
+    }
+}
---
+++ ВерюНеВерю/ВерюНеВерю/Questions.cs
@@ -0,0 +1,23 @@
+﻿namespace ВерюНеВерю
+{
+    public class Questions
+    {
+        public int Number { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+        public string Comment { get; set; }
+        public static Questions CsvLineParse(string csvLine)
+        {
+            string[] parts = csvLine.Split(';');
+            return new Questions
+            {
+                Number = int.Parse(parts[0]),
+                Question = parts[1].Trim(),
+                Answer = parts[2].Trim(),
+                Comment = parts[3].Trim()
+            };
+
+        }
+
+    }
+}
