dc421c896df400669f83b4643fe328da89155796
GeekHolo <artem.avetis@yandex.ru>
2021-05-18 17:32:44 +0300

    Мультиинструмент
    Программа для мультиинтсрументального станка с ЧПУ

---
+++ Создание программ для ЧПУ с двумя головами/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Создание_программ_для_ЧПУ_с_двумя_головами
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.TextBoxFirstProgrammName = new System.Windows.Forms.TextBox();
+            this.TextBoxSecondProgrammName = new System.Windows.Forms.TextBox();
+            this.TextBoxResultName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog3 = new System.Windows.Forms.OpenFileDialog();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip2 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip3 = new System.Windows.Forms.ToolTip(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TextBoxFirstProgrammName
+            // 
+            this.TextBoxFirstProgrammName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TextBoxFirstProgrammName.Location = new System.Drawing.Point(25, 27);
+            this.TextBoxFirstProgrammName.Name = "TextBoxFirstProgrammName";
+            this.TextBoxFirstProgrammName.Size = new System.Drawing.Size(507, 24);
+            this.TextBoxFirstProgrammName.TabIndex = 0;
+            // 
+            // TextBoxSecondProgrammName
+            // 
+            this.TextBoxSecondProgrammName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TextBoxSecondProgrammName.Location = new System.Drawing.Point(25, 85);
+            this.TextBoxSecondProgrammName.Name = "TextBoxSecondProgrammName";
+            this.TextBoxSecondProgrammName.Size = new System.Drawing.Size(507, 24);
+            this.TextBoxSecondProgrammName.TabIndex = 1;
+            // 
+            // TextBoxResultName
+            // 
+            this.TextBoxResultName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TextBoxResultName.Location = new System.Drawing.Point(25, 159);
+            this.TextBoxResultName.Name = "TextBoxResultName";
+            this.TextBoxResultName.Size = new System.Drawing.Size(507, 24);
+            this.TextBoxResultName.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(25, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(374, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Введите имя для сохранения созданной программы";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(576, 137);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(106, 70);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Выбрать папку для сохранения";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "Имя первой программы";
+            // 
+            // button
+            // 
+            this.button.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button.Location = new System.Drawing.Point(538, 16);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(172, 47);
+            this.button.TabIndex = 13;
+            this.button.Text = "Выбрать первый файл";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(538, 75);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(172, 44);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Выбрать второй    файл";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "Имя второй программы";
+            // 
+            // openFileDialog3
+            // 
+            this.openFileDialog3.FileName = "openFileDialog3";
+            this.openFileDialog3.InitialDirectory = "resultFileName";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Image = global::Создание_программ_для_ЧПУ_с_двумя_головами.Properties.Resources.Безымянный_11;
+            this.pictureBox1.Location = new System.Drawing.Point(25, 193);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(22, 28);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(50, 200);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(335, 17);
+            this.checkBox1.TabIndex = 17;
+            this.checkBox1.Text = "Открыть созданный файл в NC Corrector после его создания";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(732, 230);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TextBoxResultName);
+            this.Controls.Add(this.TextBoxSecondProgrammName);
+            this.Controls.Add(this.TextBoxFirstProgrammName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Конвертер под мультиинструмент";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextBoxFirstProgrammName;
+        private System.Windows.Forms.TextBox TextBoxSecondProgrammName;
+        private System.Windows.Forms.TextBox TextBoxResultName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog3;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ToolTip toolTip2;
+        private System.Windows.Forms.ToolTip toolTip3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Создание программ для ЧПУ с двумя головами/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+using System.Windows.Forms;
+
+namespace Создание_программ_для_ЧПУ_с_двумя_головами
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            TextBoxFirstProgrammName.Text = openFileDialog1.FileName;
+            TextBoxSecondProgrammName.Text = openFileDialog2.FileName;
+            TextBoxResultName.Text = "new file";
+            ToolTip tp = new ToolTip();
+            tp.SetToolTip(button, openFileDialog1.FileName);
+            tp.SetToolTip(button2, openFileDialog2.FileName);
+
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (TextBoxResultName.TextLength != 0)
+            {
+               
+                folderBrowserDialog1.ShowDialog();
+                string path = AppContext.BaseDirectory;
+
+                string folder = folderBrowserDialog1.SelectedPath;
+                bool nameIsAvalible = false;
+                int i = 1;
+                while (nameIsAvalible != true)
+                {
+                    
+                    if (File.Exists(folder + $"\\{TextBoxResultName.Text}.NC"))
+                    {
+                        TextBoxResultName.Text = $"new file({i})";
+                        i++;
+                    }
+                    else
+                        nameIsAvalible = true;
+
+                }
+                string resultFileName = folder + "\\" + TextBoxResultName.Text + ".NC";
+                
+                string[] pattern = File.ReadAllLines("Шаблон.txt");
+                string[] firstCode = File.ReadAllLines(openFileDialog1.FileName);
+                string[] secondCode = File.ReadAllLines(openFileDialog2.FileName);
+
+                if (File.Exists(openFileDialog1.FileName) != true || File.Exists(openFileDialog2.FileName) !=true)
+                    MessageBox.Show("Неправильно указан путь к одному из выбранных файлов");
+                else if (File.Exists("Шаблон.txt") != true)
+                    MessageBox.Show("Отсутсвует файл '\'Шаблон'\'");
+                else
+                {
+                    string[] firstCodeAfterCut = firstCode.SkipWhile(x => x.StartsWith("G0") != true)
+                                                     .TakeWhile(x => x.StartsWith("M") != true)
+                                                     .ToArray();
+
+                    string[] secondCodeAfterCut = secondCode.SkipWhile(x => x.StartsWith("G0") != true)
+                                                            .TakeWhile(x => x.StartsWith("M") != true)
+                                                            .ToArray();
+
+                    string[] firstPatternPart = pattern.TakeWhile(x => x.StartsWith(" ") != true)
+                                                            .ToArray();
+
+                    string[] secondPatternPart = pattern.SkipWhile(x => x.StartsWith("M06 T2") != true)
+                                                        .TakeWhile(x => x.StartsWith(" ") != true)
+                                                        .ToArray();
+
+                    string[] lastPatterPart = pattern.SkipWhile(x => x.StartsWith("G80") != true)
+                                                     .Select(x => x)
+                                                     .ToArray();
+                    if (File.Exists(resultFileName) == true)
+                    {
+                        MessageBox.Show("Измените имя файла или место его сохранения. Файл с указанным именем уже существует в выбранной вами директории");
+                    }
+                    else
+                    {
+                        File.AppendAllLines(resultFileName, firstPatternPart);
+                        File.AppendAllLines(resultFileName, firstCodeAfterCut);
+                        File.AppendAllText(resultFileName, "M5\n");
+                        File.AppendAllLines(resultFileName, secondPatternPart);
+                        File.AppendAllLines(resultFileName, secondCodeAfterCut);
+                        File.AppendAllLines(resultFileName, lastPatterPart);
+                        Process.Start("explorer.exe", folder);
+                        
+                        ToolTip tp = new ToolTip();
+                        tp.SetToolTip(button3, resultFileName);
+                        tp.SetToolTip(TextBoxResultName, resultFileName);
+                    }
+                    if (checkBox1.Checked)
+                    {
+                        Process.Start(path+@"\NC Corrector v4.0\NC_Corrector.exe",resultFileName);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Вы не указали имя создаваемого файла");
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog2.ShowDialog();
+            int n = openFileDialog2.FileName.Split('\\').Length;
+            TextBoxSecondProgrammName.Text = openFileDialog2.FileName.Split('\\')[n-1];
+            ToolTip tp = new ToolTip();
+            tp.SetToolTip(button2, openFileDialog2.FileName);
+            tp.SetToolTip(TextBoxSecondProgrammName, openFileDialog2.FileName);
+
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            int n = openFileDialog1.FileName.Split('\\').Length;
+            TextBoxFirstProgrammName.Text = openFileDialog1.FileName.Split('\\')[n-1];
+            ToolTip tp = new ToolTip();
+            tp.SetToolTip(button, openFileDialog1.FileName);
+            tp.SetToolTip(TextBoxFirstProgrammName, openFileDialog1.FileName);
+            
+        }
+       
+    }
+}
